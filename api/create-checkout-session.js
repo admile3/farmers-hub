79ef2d28ef8e89274bd5a033eb62b335034fedbd
admile3 <adamd.miles@gmail.com>
2026-05-22 08:17:16 -0400

    Update cancel URL for checkout session

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -31,7 +31,7 @@ export default async function handler(req, res) {
         },
       ],
       success_url: `${baseUrl}/?subscription=success`,
-      cancel_url: `${baseUrl}/subscribe?subscription=canceled`,
+      cancel_url: `${baseUrl}/subscribe`,
     });
 
     return res.status(200).json({ url: session.url });
