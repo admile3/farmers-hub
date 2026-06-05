62e3f774bca068e7f6c514d11ba9e079cbbcd7f8
admile3 <adamd.miles@gmail.com>
2026-06-05 07:54:49 -0400

    Add Flower Studio visual database

---
+++ src/data/flowerVisualDatabase.js
@@ -0,0 +1,542 @@
+export const flowerVisualStyleProfile = {
+  styleName: "Minimal Stem Study",
+  basedOn: "Marigold option 5",
+  description:
+    "Photorealistic single-stem botanical product image, centered composition, warm off-white background, soft natural window light, shallow depth of field, clear glass bud vase or small clear bottle, true-to-variety foliage, realistic scale, no text, no labels, no hands.",
+  recommendedAspectRatio: "1:1",
+  recommendedFileType: "webp",
+  recommendedDisplaySize: "400x400"
+};
+
+export const flowerVisualDatabase = [
+  {
+    "id": "zinnia",
+    "name": "Zinnia",
+    "fileName": "zinnia.webp",
+    "path": "/images/flowers/zinnia.webp",
+    "alt": "Minimal stem study image of Zinnia"
+  },
+  {
+    "id": "cosmos",
+    "name": "Cosmos",
+    "fileName": "cosmos.webp",
+    "path": "/images/flowers/cosmos.webp",
+    "alt": "Minimal stem study image of Cosmos"
+  },
+  {
+    "id": "sunflower",
+    "name": "Sunflower",
+    "fileName": "sunflower.webp",
+    "path": "/images/flowers/sunflower.webp",
+    "alt": "Minimal stem study image of Sunflower"
+  },
+  {
+    "id": "celosia",
+    "name": "Celosia",
+    "fileName": "celosia.webp",
+    "path": "/images/flowers/celosia.webp",
+    "alt": "Minimal stem study image of Celosia"
+  },
+  {
+    "id": "snapdragon",
+    "name": "Snapdragon",
+    "fileName": "snapdragon.webp",
+    "path": "/images/flowers/snapdragon.webp",
+    "alt": "Minimal stem study image of Snapdragon"
+  },
+  {
+    "id": "dahlia",
+    "name": "Dahlia",
+    "fileName": "dahlia.webp",
+    "path": "/images/flowers/dahlia.webp",
+    "alt": "Minimal stem study image of Dahlia"
+  },
+  {
+    "id": "marigold",
+    "name": "Marigold",
+    "fileName": "marigold.webp",
+    "path": "/images/flowers/marigold.webp",
+    "alt": "Minimal stem study image of Marigold"
+  },
+  {
+    "id": "amaranth",
+    "name": "Amaranth",
+    "fileName": "amaranth.webp",
+    "path": "/images/flowers/amaranth.webp",
+    "alt": "Minimal stem study image of Amaranth"
+  },
+  {
+    "id": "basil",
+    "name": "Basil",
+    "fileName": "basil.webp",
+    "path": "/images/flowers/basil.webp",
+    "alt": "Minimal stem study image of Basil"
+  },
+  {
+    "id": "eucalyptus",
+    "name": "Eucalyptus",
+    "fileName": "eucalyptus.webp",
+    "path": "/images/flowers/eucalyptus.webp",
+    "alt": "Minimal stem study image of Eucalyptus"
+  },
+  {
+    "id": "statice",
+    "name": "Statice",
+    "fileName": "statice.webp",
+    "path": "/images/flowers/statice.webp",
+    "alt": "Minimal stem study image of Statice"
+  },
+  {
+    "id": "strawflower",
+    "name": "Strawflower",
+    "fileName": "strawflower.webp",
+    "path": "/images/flowers/strawflower.webp",
+    "alt": "Minimal stem study image of Strawflower"
+  },
+  {
+    "id": "gomphrena",
+    "name": "Gomphrena",
+    "fileName": "gomphrena.webp",
+    "path": "/images/flowers/gomphrena.webp",
+    "alt": "Minimal stem study image of Gomphrena"
+  },
+  {
+    "id": "scabiosa",
+    "name": "Scabiosa",
+    "fileName": "scabiosa.webp",
+    "path": "/images/flowers/scabiosa.webp",
+    "alt": "Minimal stem study image of Scabiosa"
+  },
+  {
+    "id": "bachelor-button",
+    "name": "Bachelor Button",
+    "fileName": "bachelor-button.webp",
+    "path": "/images/flowers/bachelor-button.webp",
+    "alt": "Minimal stem study image of Bachelor Button"
+  },
+  {
+    "id": "nigella",
+    "name": "Nigella",
+    "fileName": "nigella.webp",
+    "path": "/images/flowers/nigella.webp",
+    "alt": "Minimal stem study image of Nigella"
+  },
+  {
+    "id": "calendula",
+    "name": "Calendula",
+    "fileName": "calendula.webp",
+    "path": "/images/flowers/calendula.webp",
+    "alt": "Minimal stem study image of Calendula"
+  },
+  {
+    "id": "sweet-pea",
+    "name": "Sweet Pea",
+    "fileName": "sweet-pea.webp",
+    "path": "/images/flowers/sweet-pea.webp",
+    "alt": "Minimal stem study image of Sweet Pea"
+  },
+  {
+    "id": "larkspur",
+    "name": "Larkspur",
+    "fileName": "larkspur.webp",
+    "path": "/images/flowers/larkspur.webp",
+    "alt": "Minimal stem study image of Larkspur"
+  },
+  {
+    "id": "stock",
+    "name": "Stock",
+    "fileName": "stock.webp",
+    "path": "/images/flowers/stock.webp",
+    "alt": "Minimal stem study image of Stock"
+  },
+  {
+    "id": "lisianthus",
+    "name": "Lisianthus",
+    "fileName": "lisianthus.webp",
+    "path": "/images/flowers/lisianthus.webp",
+    "alt": "Minimal stem study image of Lisianthus"
+  },
+  {
+    "id": "rudbeckia",
+    "name": "Rudbeckia",
+    "fileName": "rudbeckia.webp",
+    "path": "/images/flowers/rudbeckia.webp",
+    "alt": "Minimal stem study image of Rudbeckia"
+  },
+  {
+    "id": "black-eyed-susan",
+    "name": "Black-Eyed Susan",
+    "fileName": "black-eyed-susan.webp",
+    "path": "/images/flowers/black-eyed-susan.webp",
+    "alt": "Minimal stem study image of Black-Eyed Susan"
+  },
+  {
+    "id": "coneflower",
+    "name": "Coneflower",
+    "fileName": "coneflower.webp",
+    "path": "/images/flowers/coneflower.webp",
+    "alt": "Minimal stem study image of Coneflower"
+  },
+  {
+    "id": "yarrow",
+    "name": "Yarrow",
+    "fileName": "yarrow.webp",
+    "path": "/images/flowers/yarrow.webp",
+    "alt": "Minimal stem study image of Yarrow"
+  },
+  {
+    "id": "bee-balm",
+    "name": "Bee Balm",
+    "fileName": "bee-balm.webp",
+    "path": "/images/flowers/bee-balm.webp",
+    "alt": "Minimal stem study image of Bee Balm"
+  },
+  {
+    "id": "mountain-mint",
+    "name": "Mountain Mint",
+    "fileName": "mountain-mint.webp",
+    "path": "/images/flowers/mountain-mint.webp",
+    "alt": "Minimal stem study image of Mountain Mint"
+  },
+  {
+    "id": "goldenrod",
+    "name": "Goldenrod",
+    "fileName": "goldenrod.webp",
+    "path": "/images/flowers/goldenrod.webp",
+    "alt": "Minimal stem study image of Goldenrod"
+  },
+  {
+    "id": "aster",
+    "name": "Aster",
+    "fileName": "aster.webp",
+    "path": "/images/flowers/aster.webp",
+    "alt": "Minimal stem study image of Aster"
+  },
+  {
+    "id": "phlox",
+    "name": "Phlox",
+    "fileName": "phlox.webp",
+    "path": "/images/flowers/phlox.webp",
+    "alt": "Minimal stem study image of Phlox"
+  },
+  {
+    "id": "peony",
+    "name": "Peony",
+    "fileName": "peony.webp",
+    "path": "/images/flowers/peony.webp",
+    "alt": "Minimal stem study image of Peony"
+  },
+  {
+    "id": "tulip",
+    "name": "Tulip",
+    "fileName": "tulip.webp",
+    "path": "/images/flowers/tulip.webp",
+    "alt": "Minimal stem study image of Tulip"
+  },
+  {
+    "id": "daffodil",
+    "name": "Daffodil",
+    "fileName": "daffodil.webp",
+    "path": "/images/flowers/daffodil.webp",
+    "alt": "Minimal stem study image of Daffodil"
+  },
+  {
+    "id": "ranunculus",
+    "name": "Ranunculus",
+    "fileName": "ranunculus.webp",
+    "path": "/images/flowers/ranunculus.webp",
+    "alt": "Minimal stem study image of Ranunculus"
+  },
+  {
+    "id": "anemone",
+    "name": "Anemone",
+    "fileName": "anemone.webp",
+    "path": "/images/flowers/anemone.webp",
+    "alt": "Minimal stem study image of Anemone"
+  },
+  {
+    "id": "iceland-poppy",
+    "name": "Iceland Poppy",
+    "fileName": "iceland-poppy.webp",
+    "path": "/images/flowers/iceland-poppy.webp",
+    "alt": "Minimal stem study image of Iceland Poppy"
+  },
+  {
+    "id": "feverfew",
+    "name": "Feverfew",
+    "fileName": "feverfew.webp",
+    "path": "/images/flowers/feverfew.webp",
+    "alt": "Minimal stem study image of Feverfew"
+  },
+  {
+    "id": "queen-anne-s-lace",
+    "name": "Queen Anne's Lace",
+    "fileName": "queen-anne-s-lace.webp",
+    "path": "/images/flowers/queen-anne-s-lace.webp",
+    "alt": "Minimal stem study image of Queen Anne's Lace"
+  },
+  {
+    "id": "dara",
+    "name": "Dara",
+    "fileName": "dara.webp",
+    "path": "/images/flowers/dara.webp",
+    "alt": "Minimal stem study image of Dara"
+  },
+  {
+    "id": "bupleurum",
+    "name": "Bupleurum",
+    "fileName": "bupleurum.webp",
+    "path": "/images/flowers/bupleurum.webp",
+    "alt": "Minimal stem study image of Bupleurum"
+  },
+  {
+    "id": "orlaya",
+    "name": "Orlaya",
+    "fileName": "orlaya.webp",
+    "path": "/images/flowers/orlaya.webp",
+    "alt": "Minimal stem study image of Orlaya"
+  },
+  {
+    "id": "ageratum",
+    "name": "Ageratum",
+    "fileName": "ageratum.webp",
+    "path": "/images/flowers/ageratum.webp",
+    "alt": "Minimal stem study image of Ageratum"
+  },
+  {
+    "id": "cleome",
+    "name": "Cleome",
+    "fileName": "cleome.webp",
+    "path": "/images/flowers/cleome.webp",
+    "alt": "Minimal stem study image of Cleome"
+  },
+  {
+    "id": "verbena",
+    "name": "Verbena",
+    "fileName": "verbena.webp",
+    "path": "/images/flowers/verbena.webp",
+    "alt": "Minimal stem study image of Verbena"
+  },
+  {
+    "id": "salvia",
+    "name": "Salvia",
+    "fileName": "salvia.webp",
+    "path": "/images/flowers/salvia.webp",
+    "alt": "Minimal stem study image of Salvia"
+  },
+  {
+    "id": "sage",
+    "name": "Sage",
+    "fileName": "sage.webp",
+    "path": "/images/flowers/sage.webp",
+    "alt": "Minimal stem study image of Sage"
+  },
+  {
+    "id": "mint",
+    "name": "Mint",
+    "fileName": "mint.webp",
+    "path": "/images/flowers/mint.webp",
+    "alt": "Minimal stem study image of Mint"
+  },
+  {
+    "id": "rosemary",
+    "name": "Rosemary",
+    "fileName": "rosemary.webp",
+    "path": "/images/flowers/rosemary.webp",
+    "alt": "Minimal stem study image of Rosemary"
+  },
+  {
+    "id": "lavender",
+    "name": "Lavender",
+    "fileName": "lavender.webp",
+    "path": "/images/flowers/lavender.webp",
+    "alt": "Minimal stem study image of Lavender"
+  },
+  {
+    "id": "dusty-miller",
+    "name": "Dusty Miller",
+    "fileName": "dusty-miller.webp",
+    "path": "/images/flowers/dusty-miller.webp",
+    "alt": "Minimal stem study image of Dusty Miller"
+  },
+  {
+    "id": "scented-geranium",
+    "name": "Scented Geranium",
+    "fileName": "scented-geranium.webp",
+    "path": "/images/flowers/scented-geranium.webp",
+    "alt": "Minimal stem study image of Scented Geranium"
+  },
+  {
+    "id": "bells-of-ireland",
+    "name": "Bells of Ireland",
+    "fileName": "bells-of-ireland.webp",
+    "path": "/images/flowers/bells-of-ireland.webp",
+    "alt": "Minimal stem study image of Bells of Ireland"
+  },
+  {
+    "id": "love-in-a-puff",
+    "name": "Love-in-a-Puff",
+    "fileName": "love-in-a-puff.webp",
+    "path": "/images/flowers/love-in-a-puff.webp",
+    "alt": "Minimal stem study image of Love-in-a-Puff"
+  },
+  {
+    "id": "hyacinth-bean",
+    "name": "Hyacinth Bean",
+    "fileName": "hyacinth-bean.webp",
+    "path": "/images/flowers/hyacinth-bean.webp",
+    "alt": "Minimal stem study image of Hyacinth Bean"
+  },
+  {
+    "id": "chinese-forget-me-not",
+    "name": "Chinese Forget-Me-Not",
+    "fileName": "chinese-forget-me-not.webp",
+    "path": "/images/flowers/chinese-forget-me-not.webp",
+    "alt": "Minimal stem study image of Chinese Forget-Me-Not"
+  },
+  {
+    "id": "campanula",
+    "name": "Campanula",
+    "fileName": "campanula.webp",
+    "path": "/images/flowers/campanula.webp",
+    "alt": "Minimal stem study image of Campanula"
+  },
+  {
+    "id": "digitalis",
+    "name": "Digitalis",
+    "fileName": "digitalis.webp",
+    "path": "/images/flowers/digitalis.webp",
+    "alt": "Minimal stem study image of Digitalis"
+  },
+  {
+    "id": "hollyhock",
+    "name": "Hollyhock",
+    "fileName": "hollyhock.webp",
+    "path": "/images/flowers/hollyhock.webp",
+    "alt": "Minimal stem study image of Hollyhock"
+  },
+  {
+    "id": "delphinium",
+    "name": "Delphinium",
+    "fileName": "delphinium.webp",
+    "path": "/images/flowers/delphinium.webp",
+    "alt": "Minimal stem study image of Delphinium"
+  },
+  {
+    "id": "gladiolus",
+    "name": "Gladiolus",
+    "fileName": "gladiolus.webp",
+    "path": "/images/flowers/gladiolus.webp",
+    "alt": "Minimal stem study image of Gladiolus"
+  },
+  {
+    "id": "hydrangea",
+    "name": "Hydrangea",
+    "fileName": "hydrangea.webp",
+    "path": "/images/flowers/hydrangea.webp",
+    "alt": "Minimal stem study image of Hydrangea"
+  },
+  {
+    "id": "ninebark",
+    "name": "Ninebark",
+    "fileName": "ninebark.webp",
+    "path": "/images/flowers/ninebark.webp",
+    "alt": "Minimal stem study image of Ninebark"
+  },
+  {
+    "id": "willow",
+    "name": "Willow",
+    "fileName": "willow.webp",
+    "path": "/images/flowers/willow.webp",
+    "alt": "Minimal stem study image of Willow"
+  },
+  {
+    "id": "curly-willow",
+    "name": "Curly Willow",
+    "fileName": "curly-willow.webp",
+    "path": "/images/flowers/curly-willow.webp",
+    "alt": "Minimal stem study image of Curly Willow"
+  },
+  {
+    "id": "dogwood-branches",
+    "name": "Dogwood Branches",
+    "fileName": "dogwood-branches.webp",
+    "path": "/images/flowers/dogwood-branches.webp",
+    "alt": "Minimal stem study image of Dogwood Branches"
+  },
+  {
+    "id": "bittersweet",
+    "name": "Bittersweet",
+    "fileName": "bittersweet.webp",
+    "path": "/images/flowers/bittersweet.webp",
+    "alt": "Minimal stem study image of Bittersweet"
+  },
+  {
+    "id": "pumpkin-on-a-stick",
+    "name": "Pumpkin-on-a-Stick",
+    "fileName": "pumpkin-on-a-stick.webp",
+    "path": "/images/flowers/pumpkin-on-a-stick.webp",
+    "alt": "Minimal stem study image of Pumpkin-on-a-Stick"
+  },
+  {
+    "id": "safflower",
+    "name": "Safflower",
+    "fileName": "safflower.webp",
+    "path": "/images/flowers/safflower.webp",
+    "alt": "Minimal stem study image of Safflower"
+  },
+  {
+    "id": "ornamental-millet",
+    "name": "Ornamental Millet",
+    "fileName": "ornamental-millet.webp",
+    "path": "/images/flowers/ornamental-millet.webp",
+    "alt": "Minimal stem study image of Ornamental Millet"
+  },
+  {
+    "id": "broom-corn",
+    "name": "Broom Corn",
+    "fileName": "broom-corn.webp",
+    "path": "/images/flowers/broom-corn.webp",
+    "alt": "Minimal stem study image of Broom Corn"
+  },
+  {
+    "id": "ornamental-grass",
+    "name": "Ornamental Grass",
+    "fileName": "ornamental-grass.webp",
+    "path": "/images/flowers/ornamental-grass.webp",
+    "alt": "Minimal stem study image of Ornamental Grass"
+  },
+  {
+    "id": "pampas-grass",
+    "name": "Pampas Grass",
+    "fileName": "pampas-grass.webp",
+    "path": "/images/flowers/pampas-grass.webp",
+    "alt": "Minimal stem study image of Pampas Grass"
+  }
+];
+
+export function buildFlowerImagePrompt(flowerName, colors = "") {
+  const colorNote = colors ? ` Typical colors: ${colors}.` : "";
+
+  return `Photorealistic minimal stem study of ${flowerName}, shown as one clean representative cut stem in a small clear glass bud vase on a warm off-white background. Match a botanical catalog product image: centered composition, soft natural window light, shallow depth of field, neutral cream backdrop, realistic stem and foliage, accurate flower form, no text, no labels, no hands, no extra props, square crop.${colorNote}`;
+}
+
+export function getFlowerVisualByName(name) {
+  if (!name) return null;
+  const normalized = String(name).trim().toLowerCase();
+
+  return (
+    flowerVisualDatabase.find(
+      (flower) => flower.name.toLowerCase() === normalized
+    ) || null
+  );
+}
+
+export function getFlowerVisualById(id) {
+  if (!id) return null;
+  return flowerVisualDatabase.find((flower) => flower.id === id) || null;
+}
+
+export function getFlowerImagePath(name) {
+  return getFlowerVisualByName(name)?.path || "";
+}
